748fc94ad56ca96cb84c154e1c79de82ad39fbee
august-ag <chindaqute@gmail.com>
2026-05-02 00:05:24 +0800

    with classmates console.log

--- MyFirstApp/app/(tabs)/index.tsx
+++ MyFirstApp/app/(tabs)/index.tsx
@@ -1,23 +1,93 @@
 import { Image } from 'expo-image';
-import { StyleSheet } from 'react-native';
-import { View, Text } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
-export default function App() {
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
   return (
-    <View style={s.screen}>
-      <Image
-        source={{ url: 'https://scontent.fceb1-4.fna.fbcdn.net/v/t39.30808-6/465777034_582512961121249_8697572000580023545_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeHuUHC0O3ZTbPg_J59U9FcWh18Wi6Bs3NuHXxaLoGzc2112OK_KiJA7Qzq2aQ1Z-khgH7l_6UAouFFTh-NIuWxB&_nc_ohc=ylMDiq6mgQAQ7kNvwFZIrOY&_nc_oc=AdqgjsCWAtvZ8n1tj8pVUm6p2h7g8RFqBnn46VLWJEQjJcrzh8gxJPUTM2gCrZRZIhc&_nc_zt=23&_nc_ht=scontent.fceb1-4.fna&_nc_gid=gGM63r8UXkzoA-Jkagl6VA&_nc_ss=7b2a8&oh=00_Af7ctugTIAenYK0sivVeTH2lNm6LbzQzwBeh46nS1uB3Xg&oe=69FA83B7' }}
-        style={s.photo}
-      />
-      <Text style={s.name}>Lady Amariah Francine Daquis</Text>
-      <Text style={s.bio}>MMA Student | CS126</Text>
-    </View>
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Haro Everynyan!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">My name is August!</ThemedText>
+        <ThemedText>
+          This is me changing the text in VS Code so it can be shown on the Expo App. It's currently 9:00 PM on April 13, and I'm hoping I can pass this on time.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <Link href="/modal">
+          <Link.Trigger>
+            <ThemedText type="subtitle">My favorite game?</ThemedText>
+          </Link.Trigger>
+          <Link.Preview />
+          <Link.Menu>
+            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
+            <Link.MenuAction
+              title="Share"
+              icon="square.and.arrow.up"
+              onPress={() => alert('Share pressed')}
+            />
+            <Link.Menu title="More" icon="ellipsis">
+              <Link.MenuAction
+                title="Delete"
+                icon="trash"
+                destructive
+                onPress={() => alert('Delete pressed')}
+              />
+            </Link.Menu>
+          </Link.Menu>
+        </Link>
+
+        <ThemedText>
+          {`Since we're talking about app development, I would like to share my fav game as of late. It's called Tomodachi Life on the Nintendo 3DS. Three days from now, a part 2 of the game will be released and I can't wait for it because it's super cute!`}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">That is all :P</ThemedText>
+        <ThemedText>
+          {`That's it, I changed the text :PP Thank you all!`}
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
   );
 }
 
-const s = StyleSheet.create({
-  screen: { flex:1, alignItems: 'center', justifyContent: 'center' },
-  photo: { width:120, height:120, borderRadius:60},
-  name: { fontSize:22, fontWeight:'bold', marginTop:12},
-  bio: { fontSize:14, color: '#888'}
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
 });
+
+const myName = "August";
+const greet = (name) => {
+ return `This is my world. Welcome, ${name}!`;
+};
+const classmates = ["Khiem", "Chelsea", "Kayela", "Krismarie", "Rei", "Gaven", "Lore"];
+classmates.map(name => greet(name));
+console.log(classmates.map(name => greet(name)));
