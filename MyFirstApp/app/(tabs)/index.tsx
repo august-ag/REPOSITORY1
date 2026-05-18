1c3ee253b471cddba33ff73a4a5cb1c814d3e880
august-ag <chindaqute@gmail.com>
2026-05-18 22:09:25 +0800

    fix 2

--- MyFirstApp/app/(tabs)/index.tsx
+++ MyFirstApp/app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function App() {
   
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Image source={{ uri: 'https://scontent.fdvo1-2.fna.fbcdn.net/v/t39.30808-6/685565911_1002007175838490_3390482727752603286_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeGGPxjr67w48K5FYkBfpiGI8SLQ0Ac_v7TxItDQBz-_tEbQDzzaKJx02Se3NrkeWOBryszwMgmhgdE08FRvcJLW&_nc_ohc=ozU2Pmwt7nEQ7kNvwGxJZaJ&_nc_oc=AdpThyRMKWuqGTjwrX2GgiRq9xHd9UZ2GVQkFt_RhjGn-mdjUS3jH-JHXY8c_9_rz3k&_nc_zt=23&_nc_ht=scontent.fdvo1-2.fna&_nc_gid=nJvHVacht9nmDVFUjR6yBw&_nc_ss=7b2a8&oh=00_Af4WkJJFke_LDbzpMeeZpTsQshqPGngpAmbXNXvYLzTXdA&oe=69FA89B7' }} style={styles.image}/>
+      <Image source={{ uri: 'https://scontent.fceb1-1.fna.fbcdn.net/v/t39.30808-6/685565911_1002007175838490_3390482727752603286_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGGPxjr67w48K5FYkBfpiGI8SLQ0Ac_v7TxItDQBz-_tEbQDzzaKJx02Se3NrkeWOBryszwMgmhgdE08FRvcJLW&_nc_ohc=Xm5bO5AQclMQ7kNvwHtsnPm&_nc_oc=Ado5GkIepsTiW-mhSIIm1fGWeiwa3hVIAJy4qOp8ROYlojOXm1NdscblDTRVoH3qUeQ&_nc_zt=23&_nc_ht=scontent.fceb1-1.fna&_nc_gid=foZXHDgQizUdxya8XPlprA&_nc_ss=7b2a8&oh=00_Af7L2YL8tMBWKcfO-46RdBbTnuR3yz1AFEz9MAvL0utwIQ&oe=6A10F337' }} style={styles.image}/>
       <TextInput placeholder="Enter your name" onChangeText={setName} style={styles.input}/>
       <Text style={styles.text}>{name === '' ? "Please enter your name" :  `Hello, ${name}!`}</Text>
       <Text style={styles.text}>{message}</Text>
@@ -27,35 +27,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    backgroundColor: '#dcdcdc',
+    backgroundColor: '#d9d9d9',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20
+    padding: 20,
   },
 
   image: {
     width: 120,
     height: 120,
     borderRadius: 60,
-    marginBottom: 20
+    marginBottom: 20,
   },
 
   input: {
     borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#f5f5f5',
     width: '80%',
     padding: 10,
     marginBottom: 10,
-    backgroundColor: 'white'
+    fontSize: 16,
   },
 
   text: {
     fontSize: 16,
     marginVertical: 5,
-    color: 'black'
+    color: 'black',
   },
 
   buttonContainer: {
     marginTop: 10,
-    width: '60%'
-  }
+    width: '60%',
+  },
 });
